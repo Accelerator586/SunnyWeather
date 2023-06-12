ebbfabdfb458bb8cac854f4a7f6aa3a1fb9c6024
Accelerator586 <45753234+Accelerator586@users.noreply.github.com>
2023-06-12 21:26:46 +0800

    Update SunnyWeatherApplication.kt
    删除敏感信息

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -6,7 +6,7 @@ import android.content.Context
 class SunnyWeatherApplication: Application() {
     companion object{
         lateinit var context: Context
-        const val TOKEN = "5Df8UMfrQgU4GwGr"
+        const val TOKEN = "XXXXX"
     }
 
     override fun onCreate() {
